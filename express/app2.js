19a21d2337fea63560ba411a129c2c54632e46ab
Sahidul Hassan <alrazon@gmail.com>
2022-06-08 13:50:15 -0500

    cleaning

---
+++ express/app2.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const ejs = require('ejs');
+const path = require('path');
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
+app.set('views', path.join(__dirname, "template"));
+
+app.listen(3000, ()=>{
+    console.log('app2.js service started');
+})
+
+app.get('/admin', (req, res, next)=>{
+    console.log('Admin landing');
+    res.render('index', {list:["apple", "banana", "pears"]});
+})
